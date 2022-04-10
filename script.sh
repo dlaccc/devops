1a53b029e10f21195bfc1d93fa3c0bcbde075dae
dlaccc <103294900+dlaccc@users.noreply.github.com>
2022-04-10 18:59:51 -0400

    update script.sh

--- script.sh
+++ script.sh
@@ -9,3 +9,16 @@ sleep 2
 echo
 echo
 echo "Would you like me to go ahead and create a personal user for you"
+read answer
+#write an if statement if the answer is yes continue if the answer is no quit
+echo "Thank you for using my services"
+echo
+echo
+sleep 3
+echo "Which one of the following would you like me to do? "
+echo "1. Create a user"
+echo "2. Create a group"
+echo "3. Create a file"
+echo "4. Create a folder"
+echo "5. Create a webserver"
+#Write a case statement that will perform any action that the user chooses
