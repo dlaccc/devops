4821ddf42a7c5c98a114fc79e7c74a7915661616
root <oluwafunsholasisi4976@yahoo.com>
2022-04-10 22:42:54 +0000

    First trial

---
+++ reade.md
@@ -0,0 +1,4 @@
+This is a read me file
+
+The script in this repo is to create users and fo some other task
+You can run the script using bash "script.sh" or "./script.sh"
---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Welcome to scripting lifestyle"
+sleep 5
+echo
+echo
+echo "If you like i can create a user with a folder for you"
+sleep 2
+echo
+echo
+echo "Would you like me to go ahead and create a personal user for you"
